e4de1ce416ad59198420a0c413b9581cb820ba1e
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-23 08:24:07 -0700

    create and read for patients appointments working

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,20 @@ Rails.application.routes.draw do
     resources :appointments
   end
 
-  get '/patients/:patient_id/p/appointments', to: "appointments#patient_home", as: 'patient_home'
-  get '/patients/:patient_id/p/appointments/:id', to:"appointments#patient_show", as: 'show_patient_appointment'
+  get  '/patients/:patient_id/p/appointments', to: 'appointments#patient_home', as: 'patient_home'
 
-  get '/patients/:patient_id/p/appointments/new', to: "appointments#patient_new", as: 'new_patient_appointment_custom'
-  post '/patients/:patient_id/p/appointments', to: "appointments#patient_create"
+  get  '/patients/:patient_id/p/appointments/new', to: 'appointments#patient_new', as: 'new_patient_appointment_custom'
+  post '/patients/:patient_id/p/appointments', to: 'appointments#patient_create'
+
+  get  '/patients/:patient_id/p/appointments/:id', to:'appointments#patient_show', as: 'show_patient_appointment'
+  
+  #edit
+  get  '/patients/:patient_id/p/appointments/:id/edit', to:'appointments#patient_edit', as: 'show_patient_appointment'
+  #update 
+  put  '/patients/:patient_id/p/appointments/:id/', to:'appointments#patient_update'
+  #destroy
+  delete '/patients/:patient_id/p/appointments/:id/', to:'appointments#patient_destroy'
+  
 
   
 
